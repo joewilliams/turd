2fa0ea93c67cde5f939a8dc732b96f4e6f2a866c
Joe Williams <joe.williams@github.com>
2014-01-13 15:50:49 -0800

    change timeout for tcp

--- lib/turd/tcp.rb
+++ lib/turd/tcp.rb
@@ -4,21 +4,20 @@ module Turd
     def self.connect(request_definition)
 
       response = String.new
-
       before = Time.now.to_f
 
-      timeout(10) {
+      begin
         TCPSocket.open(request_definition[:options][:host], request_definition[:options][:port]) do |socket|
           response = socket.gets
         end
-      }
+      rescue Errno::ETIMEDOUT
+        # timeout
+      end
 
       after = Time.now.to_f
 
-      {
-        :response => response,
-        :total_time => after - before
-      }
+      {:response => response, :total_time => after - before}
+
     end
 
   end
